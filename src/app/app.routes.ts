09137d85529dfa696d29584be30071b95c15dfb4
Pabloo044 <133960320+Pabloo044@users.noreply.github.com>
2025-04-03 10:57:15 +0200

    Pagina principal
    Creacion de la pagina principal más diseño, falta ponerla en el landing y falta poner el carrusel de productos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { HomeComponent } from './home/home.component';
 import { ProductosComponent } from './productos/productos.component';
 import { SupermercadosComponent } from './supermercados/supermercados.component';
 import { RegisterComponent } from './register/register.component';
+import { PrincipalComponent } from './principal/principal.component';
+
 
 export const routes: Routes = [
+  { path: 'principal', component: PrincipalComponent },
   { path: 'home', component: HomeComponent },
   { path: 'productos', component: ProductosComponent },
   { path: 'supermercados', component: SupermercadosComponent },
   { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  
 ];
